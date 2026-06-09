414bd8808b5f38feaef24247b511fc23a14bfd3b
grimle-04 <bientualla04@gmail.com>
2026-06-09 23:36:38 +0800

    Implement admin access restriction in frmMaintenance
    Added admin access check to frmMaintenance constructor.

--- Forms/Maintenance/frmMaintenance.cs
+++ Forms/Maintenance/frmMaintenance.cs
@@ -7,7 +7,50 @@ namespace HRApplicantSystem.Forms.Maintenance
     {
         public frmMaintenance()
         {
+            // Admin only check - Sisiguraduhin na admin lang ang makakapasok
+            if (SessionManager.CurrentRole != "admin")
+            {
+                MessageBox.Show("Access denied. Admins only.");
+                this.Close();
+                return;
+            }
+
             InitializeComponent();
+        }
+
+        private void btnDepartments_Click(object sender, EventArgs e)
+        {
+            new frmDepartments().ShowDialog();
+        }
+
+        private void btnPositions_Click(object sender, EventArgs e)
+        {
+            new frmPositions().ShowDialog();
+        }
+
+        private void btnEmploymentTypes_Click(object sender, EventArgs e)
+        {
+            new frmEmploymentTypes().ShowDialog();
+        }
+
+        private void btnRequirementTypes_Click(object sender, EventArgs e)
+        {
+            new frmRequirementTypes().ShowDialog();
+        }
+
+        private void btnInterviewTypes_Click(object sender, EventArgs e)
+        {
+            new frmInterviewTypes().ShowDialog();
+        }
+
+        private void btnAssessmentTypes_Click(object sender, EventArgs e)
+        {
+            new frmAssessmentTypes().ShowDialog();
+        }
+
+        private void btnJobVacancy_Click(object sender, EventArgs e)
+        {
+            new frmJobVacancyManagement().ShowDialog();
         }
     }
 }
